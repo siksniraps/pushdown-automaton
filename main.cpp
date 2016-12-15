637300a15c0539ced02114639e9a1d4c855e2220
siksniraps <davis.sparinskis@gmail.com>
2016-12-15 17:43:41 +0200

    more advanced and currently broken validation logic

--- main.cpp
+++ main.cpp
@@ -37,13 +37,24 @@ private:
 	struct State {
 		std::string name;
 		bool accepting;
-		std::map<TransitionKey, TransitionValue> transitions;
+		std::multimap<TransitionKey, TransitionValue> transitions;
 		State() {}
 		State(std::string name, bool accepting) : name(name), accepting(accepting){}
 		bool operator<(const State& rhs) const {
 			return name < rhs.name;
 		}
 	};
+
+	struct Configuration {
+		State *currentState;
+		std::stack<std::string> stack;
+		std::string word;
+		Configuration(State *currentState, std::stack<std::string> stack, std::string word) : currentState(currentState), stack(stack), word(word) {
+		}
+
+
+	};
+
 	std::map<std::string, State> states;
 	std::set<std::string> alphabet;
 	std::set<std::string> stackAlphabet;
@@ -52,6 +63,9 @@ private:
 	std::string initialStackSymbol;
 	
 	bool acceptsWhenEmptyStack;
+	bool good;
+	
+	const std::string epsilon;
 
 	void readLineIntoSet(std::istream &in, std::set<std::string> &s) {
 	    std::string line;
@@ -82,80 +96,132 @@ private:
 
 
 public:
+
+	PushdownAutomaton() : good(false), epsilon("e"){}
+
 	void load(std::string filename) {
 
 		std::fstream fin;
 		fin.open(filename.c_str(), std::ios::in);
 
-		//read list of states
-		std::string statesLine;
-		std::getline(fin, statesLine);
-		//read alphabet
-		readLineIntoSet(fin, alphabet);
-		//read stack alphabet
-		readLineIntoSet(fin, stackAlphabet);
-		//read initial state
-		std::getline(fin, initialStateName);
-		//read initial stack symbol
-		std::getline(fin, initialStackSymbol);
-		//read accepting states
-		std::string acceptingStatesLine;
-		std::getline(fin, acceptingStatesLine);
-		//fill set of states
-		storeStates(statesLine, acceptingStatesLine);
+		if (fin) {
+			//read list of states
+			std::string statesLine;
+			std::getline(fin, statesLine);
+			//read alphabet
+			readLineIntoSet(fin, alphabet);
+			//read stack alphabet
+			readLineIntoSet(fin, stackAlphabet);
+			//read initial state
+			std::getline(fin, initialStateName);
+			//read initial stack symbol
+			std::getline(fin, initialStackSymbol);
+			//read accepting states
+			std::string acceptingStatesLine;
+			std::getline(fin, acceptingStatesLine);
+			//fill set of states
+			storeStates(statesLine, acceptingStatesLine);
+			std::string line;
+			//read accept mode
+			std::getline(fin, line);
+			acceptsWhenEmptyStack = (line == "E");
+			//read transitions
+			//read transitions only if everything else has been read successfully
+			if (!fin.fail()) {
+				while (std::getline(fin, line)) {
+					std::stringstream iss(line);
+					std::string stateName, symbol, stackSymbol, nextStateName, nextStackSymbol;
+					iss >> stateName >> symbol >> stackSymbol >> nextStateName >> nextStackSymbol;
 
-		std::string line;
-		//read accept mode
-		std::getline(fin, line);
-		acceptsWhenEmptyStack = (line == "E");
-		//read transitions
-		while (std::getline(fin, line)) {
-			std::stringstream iss(line);
-			std::istream_iterator<std::string> begin(iss);
-			std::string stateName = *begin++;
-			std::string symbol = *begin++;
-			std::string stackSymbol = *begin++;
-			std::string nextStateName = *begin++;
-			std::string nextStackSymbol = *begin++;
-
-			addTransition(stateName, symbol, stackSymbol, nextStateName, nextStackSymbol);
+					addTransition(stateName, symbol, stackSymbol, nextStateName, nextStackSymbol);
+				}
+				//everything is read, set the automaton in good state
+				good = true;
+			}
+			fin.close();
 		}
-		fin.close();
+		
 	}
 
 	bool verify(std::string word) {
-		State *currentState = &states[initialStateName];
+		std::vector<Configuration*> configurations;
 		std::stack<std::string> stack;
 		stack.push(initialStackSymbol);
-		for (unsigned int i = 0; i < word.length(); i++) {
-			TransitionKey key(std::string(1, word.at(i)), stack.top());
-			stack.pop();
-			TransitionValue value = currentState->transitions[key];
-			currentState = &states[value.nextStateName];
-			stack.push(value.nextStackSymbol);
+		configurations.push_back(new Configuration(&states[initialStateName], stack, word));
+
+		bool valid = false;
+
+		while (!valid && configurations.size()) {
+			Configuration* config = configurations.back();
+			configurations.pop_back();
+
+
+			std::string symbol = epsilon;
+			std::vector<std::pair<std::multimap<TransitionKey, TransitionValue>::iterator, std::multimap<TransitionKey, TransitionValue>::iterator> > transitionIterators;
+			if (word.length()) {
+				std::string symbol = word.substr(0, 1);
+
+				TransitionKey symbolNoStackKey(symbol, epsilon);
+				TransitionKey symbolStackKey(symbol, stack.top());
+
+				transitionIterators.push_back(config->currentState->transitions.equal_range(symbolNoStackKey));
+				transitionIterators.push_back(config->currentState->transitions.equal_range(symbolStackKey));
+			}
+
+			TransitionKey noSymbolStackKey(epsilon, stack.top());
+			TransitionKey noSymbolNoStackKey(epsilon, epsilon);
+
+
+			transitionIterators.push_back(config->currentState->transitions.equal_range(noSymbolStackKey));
+			transitionIterators.push_back(config->currentState->transitions.equal_range(noSymbolNoStackKey));
+
+
+
+			for (unsigned int k = 0; !valid && k < transitionIterators.size(); k++) {
+				for (std::multimap<TransitionKey, TransitionValue>::iterator it = transitionIterators[k].first; !valid && it != transitionIterators[k].second; it++) {
+					Configuration *copyConfig = new Configuration(*config);
+					//change new config state
+					copyConfig->currentState = &states[it->second.nextStateName];
+					//change new config stack top symbol
+					if (it->first.stackSymbol != epsilon) {
+						stack.pop();
+					}
+					if (it->second.nextStackSymbol != epsilon) {
+						stack.push(it->second.nextStackSymbol);
+					}
+					//change new config word
+					if (it->first.symbol != epsilon) {
+						copyConfig->word = copyConfig->word.erase(0, 1);
+					}
+					configurations.push_back(copyConfig);
+
+					//check for accepting state
+					valid = acceptsWhenEmptyStack ? copyConfig->stack.empty() : copyConfig->currentState->accepting;
+
+				}
+			}
+			delete config;
 		}
+
+
 		
-		return acceptsWhenEmptyStack ? stack.empty() : currentState->accepting;
+		
+		for (int i = 0; i < configurations.size(); i++) {
+			Configuration *config = configurations.back();
+			configurations.pop_back();
+			delete config;
+			
+		}
+		return valid;
+	}
+
+	bool isGood() {
+		return good;
 	}
 };
 
 
-void promptInputWord(std::string &);
 
-
-int main() {
-
-    PushdownAutomaton automaton;
-    automaton.load("automaton.in");
-	std::string word;
-	promptInputWord(word);
-
-	std::cout << (automaton.verify(word) ? "Accepted" : "Not Accepted") << std::endl;
-
-	std::cin >> word;
-
-	return 0;
-}
 
 
 
@@ -163,3 +229,34 @@ void promptInputWord(std::string &word) {
 	std::cout << "Enter a word:\n";
 	std::getline(std::cin, word);
 }
+
+
+int main() {
+
+    PushdownAutomaton automaton;
+    automaton.load("automaton.in");
+	if (automaton.isGood()) {
+		std::string word;
+		bool exit = false;
+		do {
+			promptInputWord(word);
+			if (word == ":q") {
+				exit = true;
+			}
+			else {
+				std::cout << (automaton.verify(word) ? "Accepted" : "Not Accepted") << std::endl << std::endl;
+			}
+
+		} while (!exit);
+	}
+	else {
+		std::cout << "Bad automaton input file" << std::endl;
+	}
+	
+	
+	return 0;
+}
+
+
+
+
